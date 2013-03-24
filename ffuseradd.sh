377713db668f11e66d3e0b0ff81540f6469d7c01
sliker1 <sliker.one@radic.it>
2013-03-24 14:45:57 +0100

    Comments deletet...

--- ffuseradd.sh
+++ ffuseradd.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Author:   [credential]	helper = cache[credential]	helper = cache[credential]	helper = cache
-# File:     /home/sliker/ffuseradd.sh
-# Created:  14:30:04 01/03/2013
-# Modified: 20:48:17 06/03/2013
-# Title:    ffuseradd
-# Version:  0.1
-# License:  GPLv2 - https://www.gnu.org/licenses/gpl-2.0.html
-
-
-# vim: ff=unix:fileencoding=utf-8:ft=sh
-
 if [ "$1" = "" ]; then
     echo -e "\nUSAGE: ffuseradd <USERNAME>... [EMAIL ADDRESS]...\nUsername is\
  necessary!\nEmail address is optional."
